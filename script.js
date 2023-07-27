a8664e737c2f2a36d46ed3d19fab364efd76b620
Pradeep <pradeep.kumar@qburst.com>
2023-07-27 16:21:40 +0530

    resposive_profile

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload = function () {
   const btn = document.querySelector(".hamburger");
   const menu = document.querySelector(".mobile");
+  const close = document.querySelector(".mobile .close")
   btn.addEventListener("click", function () {
-    menu.classList.toggle("active");
+    menu.classList.add("active");
   });
+
+  close.addEventListener("click",function(){
+    menu.classList.remove("active")
+  })
+
 };
